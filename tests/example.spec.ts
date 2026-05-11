0a9d19a09f891861b39ef3d7ecd9e647b9076482
UNimushakavi <unimushakavi@janiking.com>
2026-05-11 13:34:29 -0500

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://tst.jk-fms.com/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://tst.jk-fms.com/');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
